06d3e98adc40f3d2605fe5178987502a5fe952c9
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-02-11 16:47:33 -0300

    Create Hello_World.py

---
+++ 00_Hello/Hello_World.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#This first line is required to run this code as a script in a Linux OS, it tells the system wich application to use to run this code, for this one the python3 interpreter of the system.
+#Essa primeira linha e necessario para que esse codigo possa ser executado em um ambiente Linux, ela aponta para a aplicacao que deve ser usada para executar esse codigo, no caso o interpretador de python3 do sistema.
+
+print("Hello World!")
+print("Oi Mundo!")
