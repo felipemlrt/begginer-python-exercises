f20837fb1b80452f3c0c39950503c4cb383ab91e
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-02-11 16:48:30 -0300

    Create Variables.py

---
+++ 01_Variables/Variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.6
+
+#Remenber that in python there is no need to assign a type to a variable!
+#Lembre-se que em python nao ha a necessidade de declarar um tipo para a variavel!
+
+name = "John"
+age = 21
+#Note that there is a need to put " " around text values, but for values one can just attribute them.
+#For non english speakers it is very important to not use special characters, that are not available in english, in variable names otherwise errors may occur.
+#An example of wich would be preço wich uses the ç special character.
+
+nome = "Joao"
+idade = 21
+#Note que e necessario adicionar " " envolta de valores de texto porem para numeros basta atribuir.
+#Para nao falantes de ingles e muito importante nao usar caracterres que nao existem no ingles para nome de variaveis ou podem ocorrer erros ao tentar executar o codigo.
+#Um exemplo seria preço, que usa o ç.
