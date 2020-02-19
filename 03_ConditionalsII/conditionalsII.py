b2b8c686623a5aed55c79915c830f821fcd58972
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-02-19 09:22:58 -0300

    Create conditionalsII.py

---
+++ 03_ConditionalsII/conditionalsII.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+#some more complex examples of conditionals
+#alguns exemplos mais complexos de condicionais
+
+#------------------------------------------------------------------------
+#egnlish:
+name = "Jonh"
+users = ["Jonh", "Joseph"]
+
+if name in users:
+ print ("Yes, even this works")
+
+#portugues:
+nome = "joao"
+usuarios = ["joao", "jose"]
+
+if nome in usuarios:
+ print ("Sim, ate isso funciona")
+
+#------------------------------------------------------------------------
+#english:
+alarm1 = "off"
+alarm2 = "off"
+
+if alarm1 is "off" and alarm2 is "off":
+ print ("All is ok!")
+
+#portugues
+alarme1 = "desligado"
+alarme2 = "desligado"
+
+if alarme1 is "desligado" and alarme2 is "desligado":
+ print ("Tudo ok!")
+
+#------------------------------------------------------------------------
+#english:
+power = "plugged"
+generator = "off"
+
+if power is "plugged" or generator is "on":
+ print ("We have power!")
+
+#portugues
+energia = "conectada"
+gerador = "desligado"
+
+if energia is "conectada" or gerador is "ligado":
+ print ("Temos energia!")
+
+#------------------------------------------------------------------------
