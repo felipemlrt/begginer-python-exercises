5406e5548ac3676cf95d174aaea1b0e639c47b9d
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-02-27 16:07:10 -0300

    Create functionsI.py

---
+++ 06_FuntionsI/functionsI.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.6
+
+#Functions allows programmers to avoid having to write the same code multiple times and allows re use of code
+#Funções permitem a programadores evitar re escrever o mesmo codigo diversas vezes além de permitir o re uso de codigo.
+
+#----------------------------------------------------------------------------------------------------
+#english:
+def function(): #create in block, do not forget () and :
+ print("It works!")
+
+function()#call the function
+function()#note that to repeat the function is just a matter of calling it again
+#minimal example of how to create and call a function
+
+#portugues:
+def funcao(): #declaração da função, nunca esqueça () e :
+ print("Funciona!")
+
+funcao()#chamada da função
+funcao()#note que é possível re usar o mesmo código facilmente, basta chamar de novo a função
+#Exemplo minimo de como criar e chamar uma função
