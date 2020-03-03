027b7c132b98bf37931b4b43b30eeffdefed0461
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-03-03 08:57:51 -0300

    Create loopsII.py

---
+++ 05_LoopsII/loopsII.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#while loops will continue to run while the condition remains true
+
+#------------------------------------------------------------------------
+#english
+answer = "yes"
+while answer != "no":
+  answer = input("Want to continue?")
+ 
+#portugues
+answer = "sim"
+while answer != "nao":
+  answer = input(" Quer continuar?")
+ 
+#------------------------------------------------------------------------
