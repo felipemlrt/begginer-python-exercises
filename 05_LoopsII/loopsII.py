df80e5044bd53734560fcac5fa49ab96cf27fa9c
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-03-04 15:36:35 -0300

    Update loopsII.py

--- 05_LoopsII/loopsII.py
+++ 05_LoopsII/loopsII.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 #while loops will continue to run while the condition remains true
+#laços de repetição while continuam executando enquanto a condição permanecer verdadeira
 
 #------------------------------------------------------------------------
 #english
 answer = "yes"
 while answer != "no":
-  answer = input("Want to continue?")
+ answer = input("Want to continue?")
  
 #portugues
-answer = "sim"
-while answer != "nao":
-  answer = input(" Quer continuar?")
+resposta = "sim"
+while resposta != "nao":
+ resposta = input(" Quer continuar?")
  
 #------------------------------------------------------------------------
