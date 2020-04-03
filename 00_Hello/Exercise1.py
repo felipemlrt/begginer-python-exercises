fae432f700bf4adde66a65b878ae850d65c2543c
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-03 16:32:13 -0300

    Update Exercise1.py

--- 00_Hello/Exercise1.py
+++ 00_Hello/Exercise1.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+#1)
+#Create a pyhton code that will print the number 10:
+#Crie um código em python que apresente o número 10:
+
+#2)
 #Create a python code that will print your name:
 #Crie um código que apresente o seu nome:
