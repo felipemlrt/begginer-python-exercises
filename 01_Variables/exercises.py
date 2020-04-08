e2b17ff9b0760f2740346f607838c004b939ad75
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-08 16:11:43 -0300

    Update exercises.py

--- 01_Variables/exercises.py
+++ 01_Variables/exercises.py
@@ -3,3 +3,7 @@
 #1)
 #Create a series of variables containing your name and age.
 #Crie uma série de variáveis contendo seu nome e idade.
+
+#2)
+#Create a variable that receives the number 1,25.
+#Crie uma váriavel que receba o número 1,25.
