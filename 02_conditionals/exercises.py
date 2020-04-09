7da1f16f5cb1cf5aa3034f4fafa1644011b1dff0
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-09 10:48:05 -0300

    Create exercises.py

---
+++ 02_conditionals/exercises.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+#1)
+#Create a code that will ask for the user's name and print "Hello John" if the user's name is John.
+#Crie um cdigo que pergunte ao usurio seu nome e responda "Oi Paulo" se o nome do usuário for Paulo.
