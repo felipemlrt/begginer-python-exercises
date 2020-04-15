1005b01f4635954882f8eb1907cd65c6133c8109
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-15 19:08:59 -0300

    Update exercises.py

--- 02_conditionals/exercises.py
+++ 02_conditionals/exercises.py
@@ -3,3 +3,7 @@
 #1)
 #Create a code that will ask for the user's name and print "Hello John" if the user's name is John.
 #Crie um cdigo que pergunte ao usurio seu nome e responda "Oi Paulo" se o nome do usuário for Paulo.
+
+#2)
+#Create a code that will ask the use for a number and print a message saying if its a odd or even.
+#Crie um codigo que receba um numero do usuário e diga se o numéro é par ou ímpar.
