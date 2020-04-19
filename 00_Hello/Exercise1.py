2b11e31254d5d3054246be422babe94ff9b040e9
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-19 19:15:58 -0300

    Update Exercise1.py

--- 00_Hello/Exercise1.py
+++ 00_Hello/Exercise1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 #1)
-#Create a pyhton code that will print the number 10:
-#Crie um código em python que apresente o número 10:
+#Create a pyhton code that will print the number 10 in the user's console:
+#Crie um código em python que apresente o número 10 no console do usuário:
 
 #2)
 #Create a python code that will print your name:
@@ -13,9 +13,9 @@
 #Crie um cdigo em python que apresente três frases em sequência:
 
 #4)
-#Create a code that print a phrase in parts using ","
-#Crie um código em python que apresente uma frase dividida em partes usando ","
+#Create a code that print a phrase in parts using ",":
+#Crie um código em python que apresente uma frase dividida em partes usando ",":
 
 #5)
-#Create a code that prints the result of 3 * 1,25
-#Crie um código que apresente o restulado de 3 * 1,25
+#Create a code that prints the result of 3 * 1,25:
+#Crie um código que apresente o restulado de 3 * 1,25:
