c3b62039fdbf0fc734f5e4830bba9976e092d6e7
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-21 08:40:49 -0300

    Update Execise1.py

--- 03_ConditionalsII/Execise1.py
+++ 03_ConditionalsII/Execise1.py
@@ -1,3 +1,5 @@
  #!/usr/bin/env python3
 
 #1)
+#Create a code that will alsk for the user for a name and password, then answer with "Authorized !" when these are entered.
+#Crie um código que receba do usuário um nome e senha, e responda "Autorizado !" quando estes são inseridos corretamente.
