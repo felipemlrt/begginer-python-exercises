63abb514363cec63a3d17d7caec783d4fc0f71c3
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-22 15:01:16 -0300

    Update Execise1.py

--- 03_ConditionalsII/Execise1.py
+++ 03_ConditionalsII/Execise1.py
@@ -5,4 +5,5 @@
 #Crie um código que receba do usuário um nome e senha, e responda "Autorizado !" quando estes são inseridos corretamente.
 
 #2)
-#Create a code that will receive from the user the status 0 (=off) or (1=on), of alarms A and B, and prints "All OK!" if both are off.
+#Create a code that will receive from the user the status 0(off), 1(on), of alarms A and B, and prints "All OK!" if both are off.
+#Crie um código que receba do usuário o status 0(desligado), 1(ligado), de dois alarmes A e B, e apresente "Tudo OK!" se ambos estiverem desligados.
