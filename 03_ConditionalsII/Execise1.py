18dcab37b162f1c0ff77a0d7a8ef10e1cd6ca530
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-22 14:00:11 -0300

    Update Execise1.py

--- 03_ConditionalsII/Execise1.py
+++ 03_ConditionalsII/Execise1.py
@@ -3,3 +3,6 @@
 #1)
 #Create a code that will alsk for the user for a name and password, then answer with "Authorized !" when these are entered.
 #Crie um código que receba do usuário um nome e senha, e responda "Autorizado !" quando estes são inseridos corretamente.
+
+#2)
+#Create a code that will receive from the user the status 0 (=off) or (1=on), of alarms A and B, and prints "All OK!" if both are off.
