e5bda8ef8d62163033face30c63591449fbabdae
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-04-30 09:07:18 -0300

    Update Exercise1.py

--- 04_LoopsI/Exercise1.py
+++ 04_LoopsI/Exercise1.py
@@ -4,3 +4,6 @@
 #1)
 #Create a code that contains a loop that prints the numbers from 0 to 9.
 #Crie um código que use uma estrutura de repetição para apresentar os números de 0 a 9.
+
+#2)
+#Create a code that receives a variable number of pdrocuct them prints the whole list.
