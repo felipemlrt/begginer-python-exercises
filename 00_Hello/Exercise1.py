fb301347dceb38624ad09e5374e4b52189e6ecdc
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-05-14 12:08:09 -0300

    Update Exercise1.py

--- 00_Hello/Exercise1.py
+++ 00_Hello/Exercise1.py
@@ -5,17 +5,21 @@
 #Crie um código em python que apresente o número 10 no console do usuário:
 
 #2)
+#Create a pyhton code that will print the number 1,5 in the user's console:
+#Crie um código em python que apresente o número 1,5 no console do usuário:
+
+#3)
 #Create a python code that will print your name:
 #Crie um código que apresente o seu nome:
 
-#3)
+#4)
 #Create a python code that will print three phrases in sequence:
 #Crie um cdigo em python que apresente três frases em sequência:
 
-#4)
+#5)
 #Create a code that print a phrase in parts using ",":
 #Crie um código em python que apresente uma frase dividida em partes usando ",":
 
-#5)
+#6)
 #Create a code that prints the result of 3 * 1,25:
 #Crie um código que apresente o restulado de 3 * 1,25:
