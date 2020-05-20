85968f14384009fb615f75ba99741fd2eb3b29ff
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-05-20 18:03:20 -0300

    Update Exercise1.py

--- 00_Hello/Exercise1.py
+++ 00_Hello/Exercise1.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
 #1)
-#Create a pyhton code that will print the number 10 in the user's console:
-#Crie um código em python que apresente o número 10 no console do usuário:
+#Create a pyhton code that will print the number 10 in the user's console.
+#Crie um código em python que apresente o número 10 no console do usuário.
 
 #2)
-#Create a pyhton code that will print the number 1,5 in the user's console:
-#Crie um código em python que apresente o número 1,5 no console do usuário:
+#Create a pyhton code that will print the number 1,5 in the user's console.
+#Crie um código em python que apresente o número 1,5 no console do usuário.
 
 #3)
-#Create a python code that will print your name:
-#Crie um código que apresente o seu nome:
+#Create a python code that will print your name.
+#Crie um código que apresente o seu nome.
 
 #4)
-#Create a python code that will print three phrases in sequence:
-#Crie um cdigo em python que apresente três frases em sequência:
+#Create a python code that will print three phrases in sequence.
+#Crie um cdigo em python que apresente três frases em sequência.
 
 #5)
-#Create a code that print a phrase in parts using ",":
-#Crie um código em python que apresente uma frase dividida em partes usando ",":
+#Create a code that print a phrase in parts using ",".
+#Crie um código em python que apresente uma frase dividida em partes usando ",".
 
 #6)
-#Create a code that prints the result of 3 * 1,25:
-#Crie um código que apresente o restulado de 3 * 1,25:
+#Create a code that prints the result of 3 * 1,25.
+#Crie um código que apresente o restulado de 3 * 1,25.
+
+#7)
+#Create a code that prints a phrase in parts using "+".
+#
