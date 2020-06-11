a62fe2a039c4772d24f258876f0910e4dd754b5e
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-06-10 22:44:39 -0300

    Update Exercise1.py

--- 05_LoopsII/Exercise1.py
+++ 05_LoopsII/Exercise1.py
@@ -2,4 +2,8 @@
 
 #1)
 #Create a code that prints "Well done!" if the user inputs in sequence 1, 2, 3 and 4.
+#Crie um código que apresenta "Parabéns!" caso o usuário insira em sequência os números 1, 2, 3, e 4.
+
+#2)
+#
 #
