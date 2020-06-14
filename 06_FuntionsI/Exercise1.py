6117284eac067ff358cca193e24d040d49e2d589
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-06-14 09:02:45 -0300

    Update Exercise1.py

--- 06_FuntionsI/Exercise1.py
+++ 06_FuntionsI/Exercise1.py
@@ -3,3 +3,7 @@
 #1)
 #Create a function that receives a operator ( + - * or / ), values X and Y, calculates the result and prints it. 
 #Crie uma função que receva um oprador ( + - * or / ), valores X e Y, e calcule o resultando apresentando este ao usuário.
+
+#2)
+#
+#
