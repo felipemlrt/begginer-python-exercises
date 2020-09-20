30709064a27ad95fbe616c927d8cd11a810634d0
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-09-20 06:33:04 -0300

    Create classes.py

---
+++ 08_classesI/classes.py
@@ -0,0 +1,2 @@
+#A class in pythons is a collection of functions and variables all related to a certain concept, object, functionality, etc.
+#Classes make code easier to organize, reference and re use.
