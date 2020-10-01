c0ab8594fbbe764c0b3f2d654403be790e9b6d6a
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-10-01 09:36:22 -0300

    Update Hello_World.py

--- 00_Hello/Hello_World.py
+++ 00_Hello/Hello_World.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-#This first line is required to run this code as a script in a Linux OS, it tells the system wich application to use to run this code, for this one the python3 interpreter of the system.
-#Essa primeira linha e necessario para que esse codigo possa ser executado em um ambiente Linux, ela aponta para a aplicacao que deve ser usada para executar esse codigo, no caso o interpretador de python3 do sistema.
+#This first line is required to run this code as a script in a Linux OS, it tells the system wich application to use to run this code, 
+#for this one the python3 interpreter of the system. It is also possible to use the python application directly, by tipyng "python3 Hello_World.py" on the console, 
+#for this the first line is not needed.
+#Essa primeira linha e necessario para que esse codigo possa ser executado em um ambiente Linux, ela aponta para a aplicacao que deve ser usada para executar esse codigo, 
+#no caso o interpretador de python3 do sistema. Tambem é possível executar o arquivo digitando "python3 Hello_World.py" no console,
+#caso usando este formato não é necessária essa primeira linha.
 
 print("Hello World!")
 print("Oi Mundo!")
@@ -9,7 +13,7 @@ print("Oi Mundo!")
 
 print("Error: Could not read data!")
 print("Erro: Não foi possível abrir o arquivo!)
-#print() can be used to generate outputs that help both the user and programmer understand qhat the code is doing and why.
+#print() can be used to generate outputs that help both the user and programmer understand what the code is doing and why.
 #print() pode ser usada para gerar saídas que auxiliem ao usuário e ao programador entender o que está acontecendo e o motivo.
 
 print(" First: ", " John.", " Second: ", " Mike.")
