cac1976467c5da0a570fd05b8b6bc90a27e9365e
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-12-08 15:53:34 -0300

    Update Example.py

--- 00_Hello/Example.py
+++ 00_Hello/Example.py
@@ -1,2 +1,5 @@
 #some quick examples of print
 print("Name: ", "Felipe", " ID: ", 1)
+
+#print remain usefull even in advanced code, it allows the program to pass information to the user
+print("DEBUG: starting program")
