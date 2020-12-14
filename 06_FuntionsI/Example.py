f612d1d0dd0d1fd84f0f9dc1a4f2335a9acb2c39
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-12-14 19:45:05 -0300

    Update Example.py

--- 06_FuntionsI/Example.py
+++ 06_FuntionsI/Example.py
@@ -3,3 +3,9 @@ def sum_all(list_of_numbers):
  for number in list_of_numbers:
   total = total + list_of_numbers[number]
  return total
+
+def test_value(value, test_value):
+ if value == test_value:
+  return True
+ else:
+  return False
