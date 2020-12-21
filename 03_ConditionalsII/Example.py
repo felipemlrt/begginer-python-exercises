44f399e2e4637aa8e0f49843ea35ac33b43fd775
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-12-21 10:44:07 -0300

    Update Example.py

--- 03_ConditionalsII/Example.py
+++ 03_ConditionalsII/Example.py
@@ -1,2 +1,8 @@
-if x > a:
- print("x > a")
+login = "root"
+password = "S4fet1"
+user_login = input("What is your login?")
+user_password = input("What is your password?")
+if login == user_login and password == user_password:
+ print("Login sucessful!")
+else:
+ print("Login failed. Check your login/password")
