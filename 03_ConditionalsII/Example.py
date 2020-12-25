287df5ac6aef01581e1c54fb328f508b1fedef7b
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-12-25 12:36:08 -0300

    Update Example.py

--- 03_ConditionalsII/Example.py
+++ 03_ConditionalsII/Example.py
@@ -6,3 +6,14 @@ if login == user_login and password == user_password:
  print("Login sucessful!")
 else:
  print("Login failed. Check your login/password")
+
+if login == user_login and password == user_password:
+ print("Login sucessful!")
+elif login == user_login and password != user_password:
+ print("Login failed. Check your password")
+elif login != user_login and password == user_password:
+ print("Login failed. Check your login")
+elif login != user_login and password != user_password:
+ print("Login failed. Check your login and password")
+else:
+ print("ERROR: something unexpected happened!")
