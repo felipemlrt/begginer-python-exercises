489d9d9733dcb1462b6831518ee2a7a1136a686e
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-01-01 23:51:08 -0300

    Update Example.py

--- 00_Hello/Example.py
+++ 00_Hello/Example.py
@@ -3,3 +3,5 @@ print("Name: ", "Felipe", " ID: ", 1)
 
 #print remain usefull even in advanced code, it allows the program to pass information to the user
 print("DEBUG: starting program")
+
+print("ERROR: given e-mail is not valid)
