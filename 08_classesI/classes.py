cd42a5f5bbc070e51e0daaa4d9af860b1b673022
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-11-18 18:55:53 -0300

    Update classes.py

--- 08_classesI/classes.py
+++ 08_classesI/classes.py
@@ -4,14 +4,14 @@
 #Classes tornam o codigo mais facil de organizar, referenciar e re-usar.
 
 #In python classes are simple to create
-class class_name:# This create a class named <class_name>
+class class_name:# This create a class named <class_name> # a
   
   atribute = 1 #example of the creation of an atribute
   
   def sum(x, y):#This creates a function called sum that is contained on class_name
     return x + y
   
-  def multiply(x, y):#A class can cotain multiple functions
+  def multiply(x, y):#classes may contain multiple methods
     return x * y;
 
 if __name__ == "__main__": #This is used so that the code below will only run if this file is run dictly, if this class is imported to another project it will not be run
